526f2ab65920556cb8c6ee85c9adc1d0993a32f1
sarapoba <lhk981112@gmail.com>
2026-03-16 03:46:03 +0900

    [Feat] 구글 소셜 로그인 연동 및 버튼 구현

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -32,6 +32,7 @@ const login = async () => {
 // 환경 변수 로드
 
 const kakaoRedirectUri = "http://localhost:8080/oauth2/authorization/kakao"
+const googleRedirectUri = "http://localhost:8080/oauth2/authorization/google"
 
 
 const loginWithKakao = () => {
@@ -39,6 +40,9 @@ const loginWithKakao = () => {
   window.location.href = kakaoRedirectUri;
 };
 
+const loginWithGoogle = () => {
+  window.location.href = googleRedirectUri;
+};
 
 </script>
 
@@ -107,6 +111,19 @@ const loginWithKakao = () => {
             <path d="M12 3c-4.97 0-9 3.181-9 7.105 0 2.53 1.704 4.753 4.318 6.007l-1.091 4.004c-.048.175.056.355.225.388.053.01.107.01.16-.002l4.711-3.118c.224.019.452.03.687.03 4.97 0 9-3.181 9-7.105S16.97 3 12 3z"/>
           </svg>
           Login with Kakao
+        </button>
+
+        <button 
+          @click="loginWithGoogle()"
+          class="w-full flex items-center justify-center gap-3 py-4 mt-3 bg-white border border-gray-200 hover:bg-gray-50 transition-all rounded-lg text-[#191919] text-[11px] font-bold uppercase tracking-[0.1em] shadow-sm"
+        >
+          <svg class="w-5 h-5" viewBox="0 0 24 24">
+            <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
+            <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
+            <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/>
+            <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
+          </svg>
+          Login with Google
         </button>
 
         <p id="token-result" class="text-center text-[10px] text-red-400"></p>
